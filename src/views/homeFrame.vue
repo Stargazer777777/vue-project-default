8096c62d85dfdcb010884eabcabdc82d7b3b6a84
Stargazer666666 <3032964348@qq.com>
2023-06-16 20:31:25 +0800

    bug fix

--- src/views/homeFrame.vue
+++ src/views/homeFrame.vue
@@ -1,4 +1,4 @@
-<template>Hello World</template>
+<template>HelloWorld</template>
 
 <script setup lang="ts"></script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,32 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+import Components from 'unplugin-vue-components/vite';
+import {
+  ElementPlusResolver,
+  AntDesignVueResolver,
+  VantResolver,
+  HeadlessUiResolver
+} from 'unplugin-vue-components/resolvers';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      // ui库解析器，也可以自定义
+      resolvers: [
+        ElementPlusResolver(),
+        AntDesignVueResolver(),
+        VantResolver(),
+        HeadlessUiResolver()
+      ],
+      dirs: ['src/components', 'src/views'],
+      extensions: ['vue'],
+      dts: 'src/components.d.ts'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
